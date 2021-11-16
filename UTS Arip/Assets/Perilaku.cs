7c7ddd010a30a36c28b358092b6a847f3fa3b0bf
bgkoro <90332174+ariefsuryawibowo18@users.noreply.github.com>
2021-11-16 12:21:08 +0700

    Commit

---
+++ UTS Arip/Assets/Perilaku.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Perilaku : MonoBehaviour
+{
+    
+
+    void OnCollisionEnter2D(Collision2D coll){
+if (coll.gameObject.tag == "enemyTag"){
+    Debug.Log("Game Over");
+    Die();
+    
+}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Die(){
+        Debug.Log("GAME OVER");
+        SceneManager.LoadScene ("Menu");
+    }
+}
