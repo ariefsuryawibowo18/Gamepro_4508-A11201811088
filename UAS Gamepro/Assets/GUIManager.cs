9bcc6da848965f618e27fc5159e9ca1889565a5a
bgkoro <ariefsuryawibowo99@gmail.com>
2022-01-17 23:00:01 +0700

    UAS
    UAS

---
+++ UAS Gamepro/Assets/GUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPlay() {
+        SceneManager.LoadScene("Play");
+    }
+    
+    public void OnMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UAS Gamepro/Assets/Perilaku.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Perilaku : MonoBehaviour
+{
+    public Text PowerText;
+    private string PowerNum;
+
+    void OnCollisionEnter2D(Collision2D coll){
+if (coll.gameObject.tag == "enemyTag"){
+    if(PowerNum == "Off"){
+        Debug.Log("Game Over");
+        Die();
+    }
+    else{
+        
+    }
+    
+}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PowerNum = "Off";
+        PowerText.text = "Power : " +PowerNum;
+    }
+
+    private void OnTriggerEnter2D(Collider2D Power){
+
+        if(Power.tag =="powerTag"){
+
+        PowerNum = "On";
+        Destroy(Power.gameObject);
+        PowerText.text = "Power : " +PowerNum;
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Die(){
+        Debug.Log("GAME OVER");
+        SceneManager.LoadScene ("Menu");
+    }
+}
